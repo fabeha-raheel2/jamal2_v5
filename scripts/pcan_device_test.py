0a451672f3e8ed5d3a0cf6f0052805c7fb55dca8
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2025-06-19 09:35:54 +0300

    added pcan testing files

---
+++ scripts/pcan_device_test.py
@@ -0,0 +1,25 @@
+from PCANBasic import *
+from ctypes import c_ubyte
+import time
+
+pcan = PCANBasic()
+channel = PCAN_USBBUS1  # Linux still uses this constant
+baudrate = PCAN_BAUD_1M
+
+if pcan.Initialize(channel, baudrate) != PCAN_ERROR_OK:
+    print("Failed to initialize PCAN.")
+    exit(1)
+
+msg = TPCANMsg()
+msg.ID = 0x123
+msg.LEN = 8
+msg.MSGTYPE = PCAN_MESSAGE_STANDARD
+msg.DATA = (c_ubyte * 8)(0xDE, 0xAD, 0xBE, 0xEF, 0x11, 0x22, 0x33, 0x44)
+
+result = pcan.Write(channel, msg)
+if result == PCAN_ERROR_OK:
+    print("Message sent successfully.")
+else:
+    print("Failed to send message. Error:", pcan.GetErrorText(result)[1])
+
+pcan.Uninitialize(channel)
