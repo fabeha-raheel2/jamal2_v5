c2d84a5460696bcb29fe7a63003fe4c6991e628f
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2025-06-19 09:31:50 +0300

    successfully able to subscribe joint positions as published by c locomotion controller

---
+++ scripts/c_controller_joint_positions_subscriber.py
@@ -0,0 +1,16 @@
+import rospy
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+JOINT_POSITIONS = []
+
+def position_callback(msg):
+
+    JOINT_POSITIONS = msg.points[0].positions
+
+    print(JOINT_POSITIONS)
+    print("...........................................")
+
+if __name__ == "__main__":
+    rospy.init_node("Position_Subscriber")
+    joint_position_subscriber = rospy.Subscriber('/joint_group_position_controller/command', JointTrajectory, position_callback)
+    rospy.spin()
