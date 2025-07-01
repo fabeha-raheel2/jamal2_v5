669f9e29c95df5959eea51226b4a6e788434a8c9
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2025-07-01 11:59:58 +0300

    final pcan 12 motor testing scripts - can be found in new subdirectory under scripts/ by the name of "FINAL_PCAN_SCRIPTS"

---
+++ scripts/FINAL_PCAN_SCRIPTS/QuadrupedController.py
@@ -0,0 +1,59 @@
+import rospy
+import math
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+from Quadruped_config import *
+from PcanController import *
+
+class Motor:
+    def __init__(self, name='no_name', id=1, min_value=0, max_value=90, offset=0, multiplier=1):
+        self.name = name
+        self.id = id
+        self.min_value = min_value
+        self.max_value = max_value
+        self.offset = offset
+        self.multiplier = multiplier
+
+    def adjust_position(self, pos):
+        return self.multiplier * (self.constrain(round(pos, 3)) - self.offset)
+    
+    def constrain(self, val):
+        return max(self.min_value, min(val, self.max_value))
+
+class QuadrupedController:
+    def __init__(self):
+        self.pcan_bus = PcanController()
+
+        self.joint_positions = []
+        self.motors = MOTOR_IDS
+
+        for motor in MOTOR_IDS.keys():
+            self.motors[motor] = Motor(name=motor,
+                                       id=MOTOR_IDS[motor],
+                                       min_value=math.radians(MOTOR_MIN_MAX_OFFSET_MULT[motor][0]),
+                                       max_value=math.radians(MOTOR_MIN_MAX_OFFSET_MULT[motor][1]),
+                                       offset=math.radians(MOTOR_MIN_MAX_OFFSET_MULT[motor][2]),
+                                       multiplier=MOTOR_MIN_MAX_OFFSET_MULT[motor][3])
+
+        rospy.init_node("Motor_Control_Node")
+        self.joint_position_subscriber = rospy.Subscriber('/joint_group_position_controller/command', JointTrajectory, self.position_callback)
+        self.pcan_bus.initialize()
+
+        for id in MOTOR_IDS.items():
+            self.pcan_bus.set_motor_origin(motor_id=id)
+            self.pcan_bus.enable_motor_mode(motor_id=id)
+
+    def position_callback(self, msg):
+        self.joint_positions = msg.points[0].positions
+
+    def control_loop(self):
+        try:
+            while True:
+                for motor, position in zip(self.motors.items(),self.joint_positions):
+                    feedback = self.pcan_bus.send_position(motor_id=motor.id, pos=motor.adjust_position(position))
+                    print(feedback)
+        
+        except KeyboardInterrupt:
+            print("\nDisabling motor and exiting...")
+            self.pcan_bus.clean()
+              
---
+++ scripts/FINAL_PCAN_SCRIPTS/Quadruped_config.py
@@ -0,0 +1,22 @@
+# Motor IDs
+MOTOR_IDS = {'lf_hip_joint':0x01, 'lf_upper_leg_joint':0x02, 'lf_lower_leg_joint':0x03,
+             'rf_hip_joint':0x04, 'rf_upper_leg_joint':0x05, 'rf_lower_leg_joint':0x06,
+             'lh_hip_joint':0x07, 'lh_upper_leg_joint':0x08, 'lh_lower_leg_joint':0x09,
+             'rh_hip_joint':0x0A, 'rh_upper_leg_joint':0x0B, 'rh_lower_leg_joint':0x0C}
+
+# Min, Max and offset values in degrees
+MOTOR_MIN_MAX_OFFSET_MULT = {'lf_hip_joint':(-35, 35, 0, -1), 'lf_upper_leg_joint':(-20, 90, 90, -1), 'lf_lower_leg_joint':(-153, -47, -153, 0.88),
+                             'rf_hip_joint':(-35, 35, 0, -1), 'rf_upper_leg_joint':(-20, 90, 90, 1), 'rf_lower_leg_joint':(-153, -47, -153, 0.88),
+                             'lh_hip_joint':(-35, 35, 0, 1), 'lh_upper_leg_joint':(-20, 90, 90, -1), 'lh_lower_leg_joint':(-153, -47, -153, 0.88),
+                             'rh_hip_joint':(-35, 35, 0, 1), 'rh_upper_leg_joint':(-20, 90, 90, 1), 'rh_lower_leg_joint':(-153, -47, -153, 0.88)}
+
+# Value limits
+P_MIN, P_MAX = -12.5, 12.5
+V_MIN, V_MAX = -8.0, 8.0
+KP_MIN, KP_MAX = 1.0, 100.0
+KD_MIN, KD_MAX = 0.1, 5.0
+T_MIN, T_MAX = -144.0, 144.0
+
+# Default control values
+V_IN, KP_IN, KD_IN, T_IN = 0.0, 30.0, 3.0, 1.0
+
