19bdb36170153df00266f8cf61bec4c2d5e20ffc
aeel <hassan66169@gmail.com>
2025-08-25 10:46:25 +0300

    updated config file in c_controller

--- scripts/c_controller/Quadruped_config.py
+++ scripts/c_controller/Quadruped_config.py
@@ -5,19 +5,35 @@ JOINT_NAMES = ['lf_hip_joint', 'lf_upper_leg_joint', 'lf_lower_leg_joint',
                'rh_hip_joint', 'rh_upper_leg_joint', 'rh_lower_leg_joint'
                ]
 
+##### These commented values are for backward bend >> configuration
+# # Motor IDs 
+# MOTOR_IDS = {'lf_hip_joint':0x01, 'lf_upper_leg_joint':0x02, 'lf_lower_leg_joint':0x03,
+#              'rf_hip_joint':0x04, 'rf_upper_leg_joint':0x05, 'rf_lower_leg_joint':0x06,
+#              'lh_hip_joint':0x07, 'lh_upper_leg_joint':0x08, 'lh_lower_leg_joint':0x09,
+#              'rh_hip_joint':0x0A, 'rh_upper_leg_joint':0x0B, 'rh_lower_leg_joint':0x0C
+#              }
+
+# # Min, Max and offset values in degrees 
+# MOTOR_MIN_MAX_OFFSET_MULT = {'lf_hip_joint':(-35, 35, 0, -1), 'lf_upper_leg_joint':(-20, 90, 90, -1), 'lf_lower_leg_joint':(-153, -47, -153, 0.88),
+#                              'rf_hip_joint':(-35, 35, 0, -1), 'rf_upper_leg_joint':(-20, 90, 90, 1), 'rf_lower_leg_joint':(-153, -47, -153, -0.88),
+#                              'lh_hip_joint':(-35, 35, 0, 1), 'lh_upper_leg_joint':(-20, 90, 90, -1), 'lh_lower_leg_joint':(-153, -47, -153, 0.88),
+#                              'rh_hip_joint':(-35, 35, 0, 1), 'rh_upper_leg_joint':(-20, 90, 90, 1), 'rh_lower_leg_joint':(-153, -47, -153, -0.88)
+#                              }
+
 # Motor IDs 
 MOTOR_IDS = {'lf_hip_joint':0x01, 'lf_upper_leg_joint':0x02, 'lf_lower_leg_joint':0x03,
              'rf_hip_joint':0x04, 'rf_upper_leg_joint':0x05, 'rf_lower_leg_joint':0x06,
-             'lh_hip_joint':0x07, 'lh_upper_leg_joint':0x08, 'lh_lower_leg_joint':0x09,
-             'rh_hip_joint':0x0A, 'rh_upper_leg_joint':0x0B, 'rh_lower_leg_joint':0x0C
+             'lh_hip_joint':0x07, 'lh_upper_leg_joint':0x0B, 'lh_lower_leg_joint':0x0C,
+             'rh_hip_joint':0x0A, 'rh_upper_leg_joint':0x08, 'rh_lower_leg_joint':0x09
              }
 
 # Min, Max and offset values in degrees 
-MOTOR_MIN_MAX_OFFSET_MULT = {'lf_hip_joint':(-35, 35, 0, -1), 'lf_upper_leg_joint':(-20, 90, 90, -1), 'lf_lower_leg_joint':(-153, -47, -153, 0.88),
-                             'rf_hip_joint':(-35, 35, 0, -1), 'rf_upper_leg_joint':(-20, 90, 90, 1), 'rf_lower_leg_joint':(-153, -47, -153, -0.88),
-                             'lh_hip_joint':(-35, 35, 0, 1), 'lh_upper_leg_joint':(-20, 90, 90, -1), 'lh_lower_leg_joint':(-153, -47, -153, 0.88),
-                             'rh_hip_joint':(-35, 35, 0, 1), 'rh_upper_leg_joint':(-20, 90, 90, 1), 'rh_lower_leg_joint':(-153, -47, -153, -0.88)
+MOTOR_MIN_MAX_OFFSET_MULT = {'lf_hip_joint':(-25, 25, 0, -1), 'lf_upper_leg_joint':(-20, 90, 90, -1), 'lf_lower_leg_joint':(-153, -47, -153, 0.88),
+                             'rf_hip_joint':(-25, 25, 0, -1), 'rf_upper_leg_joint':(-20, 90, 90, 1), 'rf_lower_leg_joint':(-153, -47, -153, -0.88),
+                             'lh_hip_joint':(-25, 25, 0, 1), 'lh_upper_leg_joint':(-90, 20, -90, -1), 'lh_lower_leg_joint':(47, 153, 153, 0.88),
+                             'rh_hip_joint':(-25, 25, 0, 1), 'rh_upper_leg_joint':(-90, 20, -90, 1), 'rh_lower_leg_joint':(47, 153, 153, -0.88)
                              }
+
 
 # Value limits
 P_MIN, P_MAX = -12.5, 12.5
