430abe1257fd6479f5ac3cd4deb7a49e77312a55
aeel <hassan66169@gmail.com>
2025-10-12 08:29:07 +0300

    hardware testing 12-10 1

---
+++ scripts/QUAD_SDK_SCRIPTS/HW_Testing_JamalController_QUAD_SDK.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python3
+import rospy
+import math
+import sys
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Float64MultiArray
+
+from Quadruped_config import *
+from PcanController import *
+
+class Motor:
+    def __init__(self, name='no_name', id=1, min_position=0, max_position=90, offset=0, multiplier=1, 
+                 max_torque=48, min_torque=-48,
+                 min_velocity=-8, max_velocity=8):
+        self.name = name
+        self.id = id
+        self.min_position = min_position
+        self.max_position = max_position
+        self.offset = offset
+        self.multiplier = multiplier
+        self.min_torque = min_torque
+        self.max_torque = max_torque
+        self.min_velocity = min_velocity
+        self.max_velocity = max_velocity
+        
+
+    def adjust_position(self, pos):
+        return self.multiplier * (self.constrain(val=round(pos, 3), min_val=self.min_position, max_val=self.max_position) - self.offset)
+    
+    def constrain(self, val, min_val, max_val):
+        return max(min_val, min(val, max_val))
+    
+    def readjust_position(self, pos):
+        return (pos / self.multiplier) + self.offset
+    
+    def adjust_torque(self, torque):
+        return self.multiplier * self.constrain(val=round(torque, 3), min_val=self.min_torque, max_val=self.max_torque) 
+    
+    def readjust_torque(self, torque):
+        return (torque / self.multiplier)
+    
+    def adjust_velocity(self, velocity):
+        return self.multiplier * self.constrain(val=round(velocity, 3), min_val=self.min_velocity, max_val=self.max_velocity) 
+    
+    def readjust_velocity(self, velocity):
+        return (velocity / self.multiplier)
+    
+    def __str__(self):
+        return f"Motor {self.name} with ID {self.id}"
+    
+    def __repr__(self):
+        return f"Motor {self.name} with ID {self.id}"
+
+class JamalController:
+    def __init__(self, publish_feedback=True, debug=True):
+        
+        self._debug = debug
+
+        self.pcan_bus = PcanController()
+
+        self.publish_joint_state = publish_feedback
+
+        self.joint_states = {"positions":[], "velocities":[], "torques":[], "names":[]} # feedback that is received from motors
+        self.joint_commands = {"positions":[], "velocities":[], "torques":[], "kp":[], "kd":[]} # the commands that have to be sent to the motors
+        
+        # self.joint_names = JOINT_NAMES
+
+        self.motors = MOTOR_IDS.copy()
+
+        self.last_joint_states = self.joint_states.copy()
+
+        # TO-DO: Do we still need this?
+        for motor in MOTOR_IDS.keys():
+            self.motors[motor] = Motor(name=motor,
+                                       id=MOTOR_IDS[motor],
+                                       min_position=math.radians(MOTOR_MIN_MAX_OFFSET_MULT[motor][0]),
+                                       max_position=math.radians(MOTOR_MIN_MAX_OFFSET_MULT[motor][1]),
+                                       offset=math.radians(MOTOR_MIN_MAX_OFFSET_MULT[motor][2]),
+                                       multiplier=MOTOR_MIN_MAX_OFFSET_MULT[motor][3],
+                                       min_torque=T_MIN,
+                                       max_torque=T_MAX,
+                                       min_velocity=V_MIN,
+                                       max_velocity=V_MAX)
+            
+
+        # make reverse mapping (ID → name)
+        self.ID_TO_NAME = {v: k for k, v in MOTOR_IDS.items()}
+
+        rospy.init_node("Motor_Control_Node")
+        self.joint_position_subscriber = rospy.Subscriber('/joint_controller/command', JointTrajectory, self.controller_callback)
+        
+        if self.publish_joint_state:
+            self.joint_state_publisher = rospy.Publisher('/robot_1/joint_states', JointState, queue_size=1)
+
+        self.pcan_bus.initialize()
+
+        self.feedback_positions = []
+        self.joint_names = []
+        if self.publish_joint_state:
+            if self._debug:
+                user_input = input("Set Motors 0 Position?")
+
+                if user_input == "y" or user_input == "Y":
+                    for motor in self.motors.values():
+                        self.pcan_bus.set_motor_origin(motor_id=motor.id)
+                        self.pcan_bus.enable_motor_mode(motor_id=motor.id)
+                        # self.pcan_bus.send_position(motor_id=motor.id, pos=0)
+                        self.feedback_positions.append(motor.readjust_position(pos=0))
+                        # self.joint_names.append(motor.name)
+                        self.joint_states['names'].append(motor.name)
+                        print(motor.name)
+            # else:
+            #     self.feedback_positions.append(motor.readjust_position(pos=0))
+        while True:
+            try:
+                if self.publish_joint_state:
+                    # self.joint_states = {"positions":[], "velocities":[0,0,0,0,0,0,0,0,0,0,0,0], "torques":[1,1,1,1,1,1,1,1,1,1,1,1], "names":[], "id":[]}
+                    self.joint_states['velocities'] =[0,0,0,0,0,0,0,0,0,0,0,0]
+                    self.joint_states['torques'] =[1,1,1,1,1,1,1,1,1,1,1,1]
+                    
+                    # self.publish_joint_feedback()
+                    msg = JointState()
+
+                    msg.header.stamp = rospy.Time.now()
+                    # msg.name = self.joint_names
+                    msg.name = self.joint_states['names']
+                    msg.position = self.feedback_positions
+                    msg.velocity = self.joint_states['velocities']
+                    msg.effort   = self.joint_states['torques']
+
+                    self.joint_state_publisher.publish(msg)
+            except KeyboardInterrupt:
+                break
+
+        if self.publish_joint_state:
+                # self.publish_joint_feedback()
+                msg = JointState()
+
+                msg.header.stamp = rospy.Time.now()
+                # msg.name = self.joint_names
+                msg.name = self.joint_states['names']
+                msg.position = self.feedback_positions
+
+                self.joint_state_publisher.publish(msg)
+
+    def controller_callback(self, msg):
+        self.joint_commands = {"positions":[], "velocities":[], "torques":[], "kp":[], "kd":[]}
+    
+        # self.joint_commands["positions"] = msg.points[0].positions
+        self.joint_commands["positions"] = [0] *12
+        # self.joint_commands["velocities"] = msg.points[0].velocities
+        self.joint_commands["velocities"] = [0]*12
+        self.joint_commands["kp"] = [0] *12
+        self.joint_commands["kd"] = [0] *12
+        # self.joint_commands["torques"] = msg.points[0].effort
+        self.joint_commands["torques"] = [0] *12
+
+        # print("Joint Commands: ", self.joint_commands)
+
+    def run_loop(self):
+        while not rospy.is_shutdown():
+            # Send these commands to each of the motors
+            self.send_motor_commands()
+
+            # Publish the feedback of all 12 motors
+            if self.publish_joint_state:
+                self.publish_joint_feedback()
+            
+            
+    def send_motor_commands(self):
+        self.joint_states = {"positions":[], "velocities":[], "torques":[], "names":[], "id":[]}
+        self.joint_names = []
+
+        if len(self.joint_commands["positions"]) == 12 and len(self.joint_commands["velocities"]) == 12 and len(self.joint_commands["torques"]) == 12 and len(self.joint_commands["kp"]) == 12 and len(self.joint_commands["kd"]) == 12:
+
+            for motor, position, velocity, torque, kp, kd in zip(self.motors.values(), self.joint_commands["positions"], self.joint_commands["velocities"], self.joint_commands["torques"], self.joint_commands["kp"], self.joint_commands["kd"]):
+                if position != 0:
+                    try:
+                        feedback = {"position":[], "velocity":[], "torque":[], 'id': []}
+                        feedback = self.pcan_bus.send_motor_data(motor_id=motor.id, 
+                                                                    pos=motor.adjust_position(position), 
+                                                                    v_in=motor.adjust_velocity(velocity),
+                                                                    t_in=motor.adjust_torque(torque),
+                                                                    kp_in=kp,
+                                                                    kd_in=kd)
+
+                        self.joint_states['positions'].append(motor.readjust_position(feedback['position']))
+                        self.joint_states['velocities'].append(motor.readjust_velocity(feedback['velocity']))
+                        self.joint_states['torques'].append(motor.readjust_torque(feedback['torque']))
+                        self.joint_states['names'].append(motor.name)
+                        # self.joint_states['names'].append(name)
+                        self.joint_states['id'].append((feedback['id']))
+                        # self.joint_names.append(motor.name)
+                        print(self.joint_states['id'])
+                        print(self.joint_states['names'])
+                        print(self.joint_states['positions'])
+                        
+                        
+                    except KeyboardInterrupt:
+                        print("\nDisabling motor and exiting...")
+                        
+                        for id in MOTOR_IDS.values():
+                            self.pcan_bus.disable_motor_mode(motor_id=id)
+                        
+                        self.pcan_bus.clean()
+                        break
+
+
+                else:
+                    # Send the command    
+                    try:
+                        # print("else condition")
+                        feedback = {"position":[], "velocity":[], "torque":[], 'id': []}
+                        feedback = self.pcan_bus.send_motor_data(motor_id=motor.id, 
+                                                                    pos=motor.adjust_position(position), 
+                                                                    v_in=0,
+                                                                    t_in=0,
+                                                                    kp_in=0,
+                                                                    kd_in=0)
+                        
+                        self.joint_states['positions'].append(motor.readjust_position(feedback['position']))
+                        self.joint_states['velocities'].append(motor.readjust_velocity(feedback['velocity']))
+                        self.joint_states['torques'].append(motor.readjust_torque(feedback['torque']))
+                        self.joint_states['names'].append(motor.name)
+                        # self.joint_states['names'].append(name)
+                        self.joint_states['id'].append((feedback['id']))
+                        # self.joint_names.append(motor.name)
+                        print(self.joint_states['id'])
+                        print(self.joint_states['names'])
+                        print(self.joint_states['positions'])
+                        
+                    except KeyboardInterrupt:
+                        print("\nDisabling motor and exiting...")
+                        
+                        for id in MOTOR_IDS.values():
+                            self.pcan_bus.disable_motor_mode(motor_id=id)
+                        
+                        self.pcan_bus.clean()
+                        break
+
+            # print("loop ended")
+    def publish_joint_feedback(self):
+        msg = JointState()
+
+        msg.header.stamp = rospy.Time.now()
+        # msg.name = self.joint_names
+        msg.name = self.joint_states['names']
+        msg.position = self.joint_states['positions']
+        msg.velocity = self.joint_states['velocities']
+        msg.effort = self.joint_states['torques']
+        # print(self.joint_states)
+        self.joint_state_publisher.publish(msg)
+
+            
+              
+if __name__ == "__main__":
+    quadruped_controller = JamalController()
+    # rospy.spin()
+    quadruped_controller.run_loop()
---
+++ scripts/QUAD_SDK_SCRIPTS/Quadruped_config.py
@@ -0,0 +1,90 @@
+# Robot Joint Names 
+# JOINT_NAMES = [
+#     'RF_HAA', 'RF_HFE', 'RF_KFE',   # Right Front: Hip Abduction/Adduction, Hip Flexion/Extension, Knee Flexion/Extension
+#     'LF_HAA', 'LF_HFE', 'LF_KFE',   # Left Front
+#     'RH_HAA', 'RH_HFE', 'RH_KFE',   # Right Hind
+#     'LH_HAA', 'LH_HFE', 'LH_KFE'    # Left Hind
+# ]
+
+# JOINT_NAMES = [
+#     'LF_HAA',8 'LF_HFE',0 'LF_KFE',1   # Left Front
+#     'LH_HAA',9 'LH_HFE',2 'LH_KFE' 3,    # Left Hind
+#     'RF_HAA',10 'RF_HFE',4 'RF_KFE',5   # Right Front: Hip Abduction/Adduction, Hip Flexion/Extension, Knee Flexion/Extension
+#     'RH_HAA',11 'RH_HFE',6 'RH_KFE',7   # Right Hind
+
+# ]
+
+# Robot Joint Names 
+# JOINT_NAMES = [
+#     '8', '0', '1',   # Left Front
+#     '9', '2', '3',    # Left Hind
+#     '10', '4', '5',   # Right Front: Hip Abduction/Adduction, Hip Flexion/Extension, Knee Flexion/Extension
+#     '11', '6', '7',   # Right Hind
+
+# ]
+JOINT_NAMES = [
+    '0', '1', '10',   # Left Front
+    '11', '2', '3',    # Left Hind
+    '4', '5', '6',   # Right Front: Hip Abduction/Adduction, Hip Flexion/Extension, Knee Flexion/Extension
+    '7', '8', '9',   # Right Hind
+
+]
+
+# Motor IDs for INWARD BEND CONFIGURATION
+# MOTOR_IDS = {
+#     'LF_HAA':0x01, 'LF_HFE':0x02, 'LF_KFE':0x03,   # Left Front
+#     'LH_HAA':0x07, 'LH_HFE':0x0B, 'LH_KFE':0x0C,    # Left Hind
+#     'RF_HAA':0x04, 'RF_HFE':0x05, 'RF_KFE':0x06,   # Right Front: Hip Abduction/Adduction, Hip Flexion/Extension, Knee Flexion/Extension
+#     'RH_HAA':0x0A, 'RH_HFE':0x08, 'RH_KFE':0x09,   # Right Hind
+
+#              }
+# Motor IDs 
+# MOTOR_IDS = {
+#     '8':0x01, '0':0x02, '1':0x03,   # Left Front: Hip Abduction/Adduction, Hip Flexion/Extension, Knee Flexion/Extension
+#     '9':0x07, '2':0x08, '3':0x09,    # Left Hind
+#     '10':0x04, '4':0x05, '5':0x06,   # Right Front 
+#     '11':0x0A, '6':0x0B, '7':0x0C,   # Right Hind
+
+#              }
+
+MOTOR_IDS = {
+    '0':0x02, '1':0x03, '10':0x04,   # Left Front: Hip Abduction/Adduction, Hip Flexion/Extension, Knee Flexion/Extension
+    '11':0x0A,'2':0x08, '3':0x09,    # Left Hind
+    '4':0x05, '5':0x06, '6':0x0B, # Right Front 
+    '8':0x01,'9':0x07, '7':0x0C,   # Right Hind
+
+    }
+
+# MOTOR_INDEX = {
+#     'RF_HAA':0, 'RF_HFE':1, 'RF_KFE':2,   # Right Front: Hip Abduction/Adduction, Hip Flexion/Extension, Knee Flexion/Extension
+#     'LF_HAA':3, 'LF_HFE':4, 'LF_KFE':5,   # Left Front
+#     'RH_HAA':6, 'RH_HFE':7, 'RH_KFE':8,   # Right Hind
+#     'LH_HAA':9, 'LH_HFE':10, 'LH_KFE':11    # Left Hind
+#              }
+
+# These offesets are for QUAD_SDK
+MOTOR_MIN_MAX_OFFSET_MULT  = {
+    '8':(-25, 25, -7, -1), '0':(-90, 110, -64,  1), '1':(48, 132, 48, 0.88),   # Left Front: Hip Abduction/Adduction, Hip Flexion/Extension, Knee Flexion/Extension
+    '9':(-25, 25, -7,  1), '2':(-90, 110, -64,  1), '3':(48, 132, 48, 0.88),    # Left Hind
+    '10':(-25, 25,  7, -1), '4':(-90, 110, -64, -1), '5':(48, 132, 48, -0.88),   # Right Front
+    '11':(-25, 25,  7,  1), '6':(-90, 110, -64, -1), '7':(48, 132, 48, -0.88)   # Right Hind
+             }
+
+# MOTOR_MIN_MAX_OFFSET_MULT  = {
+#     'LF_HAA':(-25, 25, -7, -1), 'LF_HFE':(-90, 110, -64,  1), 'LF_KFE':(48, 132, 48, 0.88),   # Left Front: Hip Abduction/Adduction, Hip Flexion/Extension, Knee Flexion/Extension
+#     'LH_HAA':(-25, 25, -7,  1), 'LH_HFE':(-90, 110, -64,  1), 'LH_KFE':(48, 132, 48, 0.88),    # Left Hind
+#     'RF_HAA':(-25, 25,  7, -1), 'RF_HFE':(-90, 110, -64, -1), 'RF_KFE':(48, 132, 48, -0.88),   # Right Front
+#     'RH_HAA':(-25, 25,  7,  1), 'RH_HFE':(-90, 110, -64, -1), 'RH_KFE':(48, 132, 48, -0.88)   # Right Hind
+#              }
+
+
+# Value limits
+P_MIN, P_MAX = -12.5, 12.5
+V_MIN, V_MAX = -5.0, 5.0
+KP_MIN, KP_MAX = 1.0, 100.0
+KD_MIN, KD_MAX = 0.1, 5.0
+T_MIN, T_MAX = -144.0, 144.0
+
+# Default control values
+V_IN, KP_IN, KD_IN, T_IN = 0.0, 0, 2.0, 0.0
+
